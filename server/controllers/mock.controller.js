31dac75043a12b65b1675e606823c62503bd672c
JuanQP <quinterosp.juan@gmail.com>
2022-10-06 11:55:13 -0300

    Update mock API to match new format

--- server/controllers/mock.controller.js
+++ server/controllers/mock.controller.js
@@ -2,7 +2,7 @@ module.exports = {
   contenidosList: async (_, res) => {
     try {
       res.status(200).send({
-        data: contenidosTerror.concat(contenidosSuspenso).concat(contenidosComedia)
+        contenidos: contenidosTerror.concat(contenidosSuspenso).concat(contenidosComedia)
       });
     } catch (error) {
       res.status(400).send(error);
@@ -12,10 +12,10 @@ module.exports = {
   carruselesList: async (_, res) => {
     try {
       res.status(200).send({
-        data: [
+        carruseles: [
           {id: 1, title: "Terror", contenidos: contenidosTerror},
           {id: 2, title: "Suspenso", contenidos: contenidosSuspenso},
-          {id: 2, title: "Comedia", contenidos: contenidosComedia},
+          {id: 3, title: "Comedia", contenidos: contenidosComedia},
         ]
       });
     } catch (error) {
@@ -25,14 +25,14 @@ module.exports = {
 };
 
 const contenidosTerror = [
-  {"title": "El conjuro", description: "Descripción de ejemplo 1", year: 2013, duration: 6960, director: "James Wan", cast: "Vera Farmiga y Patrick Wilson", writer: "Chad Hayes", genres: "Terror", maturity_rating: "+16"},
-  {"title": "La monja", description: "Descripción de ejemplo 2", year: 2018, duration: 5760, director: "Corin Hardy", cast: "Taissa Farmiga y Bonnie Aarons", writer: "Gary Dauberman", genres: "Terror", maturity_rating: "+18"}
+  {"title": "El conjuro", description: "Descripción de ejemplo 1", year: 2013, duration: 116, director: "James Wan", cast: "Vera Farmiga y Patrick Wilson", writer: "Chad Hayes", genres: [{id: 1, description: "Terror"}], MaturityRating: {id: 1, description: "R - Restricted"}},
+  {"title": "La monja", description: "Descripción de ejemplo 2", year: 2018, duration: 96, director: "Corin Hardy", cast: "Taissa Farmiga y Bonnie Aarons", writer: "Gary Dauberman", genres: [{id: 1, description: "Terror"}], MaturityRating: {id: 1, description: "NC-17 - Adults Only"}}
 ];
 const contenidosSuspenso = [
-  {"title": "El colleccionista de huesos", description: "Descripción de ejemplo 3", year: 2000, duration: 7080, director: "Phillip Noyce", cast: "Denzel Washington y Angelina Jolie", writer: "Jeffery Deaver", genres: "Suspenso", maturity_rating: "ATP"},
-  {"title": "La sospecha", description: "Descripción de ejemplo 4", year: 2013, duration: 9180, director: "Denis Villeneuve", cast: "Jake Gyllenhaal y Huge Jackman", writer: "Aaron Guzikowski", genres: "Suspenso", maturity_rating: "ATP"}
+  {"title": "El colleccionista de huesos", description: "Descripción de ejemplo 3", year: 2000, duration: 7080, director: "Phillip Noyce", cast: "Denzel Washington y Angelina Jolie", writer: "Jeffery Deaver", genres: [{id: 2, description: "Suspenso"}], MaturityRating: {id: 1, description: "G - General Audiences"}},
+  {"title": "La sospecha", description: "Descripción de ejemplo 4", year: 2013, duration: 9180, director: "Denis Villeneuve", cast: "Jake Gyllenhaal y Huge Jackman", writer: "Aaron Guzikowski", genres: [{id: 2, description: "Suspenso"}], MaturityRating: {id: 1, description: "G - General Audiences"}}
 ];
 const contenidosComedia = [
-  {"title": "Pinneaple Express", description: "Descripción de ejemplo 5", year: 2008, duration: 7020, director: "David Gordon Green", cast: "Set Rogen y James Franco", writer: "Set Rogen", genres: "Comedia", maturity_rating: "ATP"},
-  {"title": "Mentiroso mentiroso", description: "Descripción de ejemplo 6", year: 1997, duration: 5220, director: "Tom Shadyac", cast: "Jim Carrey y Jennifer Tilly", writer: "Stephen Mazur", genres: "Comedia", maturity_rating: "ATP"}
+  {"title": "Pinneaple Express", description: "Descripción de ejemplo 5", year: 2008, duration: 7020, director: "David Gordon Green", cast: "Set Rogen y James Franco", writer: "Set Rogen", genres: [{id: 3, description: "Comedia"}], MaturityRating: {id: 1, description: "G - General Audiences"}},
+  {"title": "Mentiroso mentiroso", description: "Descripción de ejemplo 6", year: 1997, duration: 5220, director: "Tom Shadyac", cast: "Jim Carrey y Jennifer Tilly", writer: "Stephen Mazur", genres: [{id: 3, description: "Comedia"}], MaturityRating: {id: 1, description: "G - General Audiences"}}
 ];
