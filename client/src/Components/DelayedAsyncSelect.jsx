a8fc433794c014517c575ae98c97dc30d3e18e24
JuanQP <quinterosp.juan@gmail.com>
2022-10-06 11:09:21 -0300

    Fix bug in DelayedAsyncSelect
    Now it works if there are more than one DelayedAsyncSelect on screen

--- client/src/Components/DelayedAsyncSelect.jsx
+++ client/src/Components/DelayedAsyncSelect.jsx
@@ -1,20 +1,22 @@
+import { useRef } from 'react';
 import AsyncSelect from 'react-select/async';
 
-let timeout = null;
-
-/**
- * Load contents with delay
- */
-function loadContents(searchText, callback, fetchCallback, delay) {
-  // Reset timeout after each key stroke
-  clearTimeout(timeout);
-  // Start a timer... 
-  timeout = setTimeout(function() {
-    fetchCallback(searchText, callback);
-  }, delay);
-}
-
 export function DelayedAsyncSelect({delay, fetchCallback, ...props}) {
+  
+  const timeout = useRef(null);
+  
+  /**
+   * Load contents with delay
+   */
+  function loadContents(searchText, callback, fetchCallback, delay) {
+    // Reset timeout after each key stroke
+    clearTimeout(timeout.current);
+    // Start a timer... 
+    timeout.current = setTimeout(function() {
+      fetchCallback(searchText, callback);
+    }, delay);
+  }
+
   return (
     <AsyncSelect
       {...props}
